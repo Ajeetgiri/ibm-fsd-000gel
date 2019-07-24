4d4ec2aa9b7dec03f25620f99b63fc72a454cf78
Ajeetgiri <52523762+Ajeetgiri@users.noreply.github.com>
2019-07-24 08:49:32 +0530

    Add files via upload

---
+++ dbitem.js
@@ -0,0 +1,53 @@
+const item = [
+    {
+        id : 1,
+        name : 'Album 1',
+        cateogry : 'Music',
+        quantity : 10,
+        price : 1299,
+
+    },
+    {
+        id : 2,
+        name : 'Album 2',
+        cateogry : 'Music',
+        quantity : 3,
+        
+
+    },
+    {
+        id : 3,
+        name : 'Album 3',
+        cateogry : 'Music',
+        quantity : 23,
+        
+
+    },
+    {
+        id : 4,
+        name : 'Album 4',
+        cateogry : 'Music',
+        quantity : 18,
+
+
+    },
+    {
+        id : 5,
+        name : 'T-Shirt',
+        cateogry : 'Merch',
+        quantity : 20,
+        
+
+    },
+    {
+        id : 6,
+        name : 'Coffe Cup',
+        cateogry : 'Merch',
+        quantity : 20,
+        
+
+    }
+]
+module.exports = {
+    item
+}

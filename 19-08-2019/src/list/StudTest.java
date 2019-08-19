9388fa6e022e806854aed5f6402b52e4c9951e8d
b4ibmjava28@iiht.tech <b4ibmjava28@iiht.tech>
2019-08-19 17:38:30 +0530

    jdbc

---
+++ 19-08-2019/src/list/StudTest.java
@@ -0,0 +1,12 @@
+package list;
+
+import java.util.Scanner;
+
+public class StudTest {
+	public static void main(String[] args) {
+		System.out.println("enter your option :" + "1-Add" + "2-Delete" + "3-View" + "4-Update");
+		Scanner sc = new Scanner(System.in);
+		int option = sc.nextInt();
+	}
+
+}

36697c5bdb2d0100a2d8d685d54769dda22a04bf
b4ibmjava28@iiht.tech <b4ibmjava28@iiht.tech>
2019-08-21 20:32:09 +0530

    question 2

--- 21-08-2019/src/Problem_statement_Employee/AgeComparator.java
+++ 21-08-2019/src/Problem_statement_Employee/AgeComparator.java
@@ -1,15 +1,56 @@
 package Problem_statement_Employee;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.Set;
 
 public class AgeComparator implements Comparator{
 
-	@Override
-	public int compare(Object o1, Object o2) {
-		// TODO Auto-generated method stub
-		return 0;
+ArrayList<Employee> list1 = new ArrayList<Employee>();
+	
+
+	public AgeComparator() {
+		super();
+	}
+
+	public void setEmployee(Set<Employee> list) {
+		
+		for(Employee e :list) {
+		list1.add(e);
+		}
+		Collections.sort(list1,new AgeComparator());
+		
 	}
 	
+	public ArrayList<Employee> getEmployee(){
+		return list1;
+	}
+
+	@Override
+	public int compare(Object o1, Object o2) {
+		Employee s1 = (Employee) o1;
+		Employee s2 = (Employee) o2;
+		if (s1.getAge() == s2.getAge()) {
+			if (s1.getDate() == s2.getDate()) {
+				return 0;
+			} 
+			else if (s1.getDate().after(s2.getDate())) {
+				return 1;
+			}
+			else {
+				return -1;
+			}
+		} 
+		else if (s1.getAge() > s2.getAge()) {
+			return 1;
+		}
+		else {
+			return -1;
+		}
+	}
+	
+
 
 	
 

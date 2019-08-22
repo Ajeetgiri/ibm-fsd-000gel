0793ad9d945d2c59047bfc964ba266eb5b72e6c7
b4ibmjava28@iiht.tech <b4ibmjava28@iiht.tech>
2019-08-22 08:51:25 +0530

    problem 5

---
+++ 21-08-2019/src/Assesment/LetterSequence.java
@@ -0,0 +1,44 @@
+package Assesment;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class LetterSequence {
+	private String sentence;
+	private TreeMap<Character, Integer> frequency = new TreeMap<Character,Integer>();
+
+	public LetterSequence(String sentence) {
+		super();
+		this.sentence = sentence;
+	}
+
+	public TreeMap<Character,Integer> computeFrequency(){
+		 char[] stringArray = sentence.toCharArray();
+		 for (char c : stringArray)
+	        {
+	            if(frequency.containsKey(c))
+	            {
+	                frequency.put(c, frequency.get(c)+1);
+	            }
+	            else
+	            {
+	                frequency.put(c, 1);
+	            }
+	        }
+		 return frequency;
+		
+	}
+	
+	public void displayLetterFrequency(TreeMap<Character,Integer> frequencyMap) {
+		for(Map.Entry<Character,Integer> map : frequencyMap.entrySet()) {
+				Character key = map.getKey();
+			  Integer value = map.getValue();
+			  System.out.print(key + ":");
+			  for(int i =0 ;i<value;i++) {
+				  System.out.print('*');
+			  }
+			  System.out.println("\n");
+		}
+		
+	}
+}
---
+++ 21-08-2019/src/Assesment/Main.java
@@ -0,0 +1,16 @@
+package Assesment;
+
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Main {
+	Scanner scanner = new Scanner(System.in);
+	TreeMap<Character, Integer> frequency = new TreeMap<Character,Integer>();
+	System.out.println("Enter input string:");
+	String sentence = scanner.nextLine();
+	LetterSequence letter = new LetterSequence(sentence);
+	frequency = letter.computeFrequency();
+	letter.displayLetterFrequency(frequency);
+}
+
+}

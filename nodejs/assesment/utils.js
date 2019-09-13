21c0318524dd122376f65d2ee08d99b6ac18739a
b4ibmjava28@iiht.tech <b4ibmjava28@iiht.tech>
2019-09-13 17:29:21 +0530

    maven project

--- nodejs/assesment/utils.js
+++
@@ -1,9 +0,0 @@
-const utils = {
-    smtp : {
-        provider : 'gmail',
-        address :  'ibmtechtraining007@gmail.com',
-        password : 'India@786'
-    }
-}
-
-module.exports.utils = utils;
